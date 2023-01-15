96f6cc356aef0fe20c662df18466b1e2c2f28c74
Anthony Honstain <honstain@gmail.com>
2023-01-15 08:55:52 -0800

    Experimenting with Tabulator to list the inventory records in a nicer looking table

--- inventory/views.py
+++ inventory/views.py
@@ -6,4 +6,7 @@ from cyclecount.models import Inventory
 
 def list_inventory(request: HttpRequest) -> HttpResponse:
     inventory = Inventory.objects.select_related('product', 'location').all()
-    return render(request, 'inventory/list_inventory.html', {'inventory_records': inventory})
+    inventory_records = []
+    for inv in inventory:
+        inventory_records.append({'id': inv.id, 'location': inv.location.description, 'sku': inv.product.sku, 'qty': inv.qty})
+    return render(request, 'inventory/list_inventory.html', {'inventory_records': inventory_records})
