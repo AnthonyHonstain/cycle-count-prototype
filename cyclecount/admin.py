dfa16c61adbd3f7cf1b846c9410a9e9d0e1f413d
Anthony Honstain <honstain@gmail.com>
2023-01-15 07:09:20 -0800

    Improve admin view by adding list_display for all the models

--- cyclecount/admin.py
+++ cyclecount/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Location, Product, Inventory, CountSession, IndividualCount, CycleCountModification, CustomUser
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'sku')
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description')
+
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'qty', 'location', 'product')
+
+
 class CountSessionAdmin(admin.ModelAdmin):
     list_display = ('id', 'created_by', 'final_state', 'count_of_individual_counts')
 
@@ -11,9 +23,9 @@ class IndividualCountAdmin(admin.ModelAdmin):
 
 
 admin.site.register(CustomUser)
-admin.site.register(Location)
-admin.site.register(Product)
-admin.site.register(Inventory)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Inventory, InventoryAdmin)
 admin.site.register(CountSession, CountSessionAdmin)
 admin.site.register(IndividualCount, IndividualCountAdmin)
 admin.site.register(CycleCountModification)
