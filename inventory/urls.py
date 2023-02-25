d8bf8e5ef9be988431579e7f5289a924a61bafc6
Anthony Honstain <honstain@gmail.com>
2023-02-25 07:57:43 -0800

    Expanding the JSON call to populate the inventory, running both a DB call version and a version that gets the product information from another service, still missing tests

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'inventory'
 urlpatterns = [
     path('list_inventory/', views.list_inventory, name='list_inventory'),
-    path('inventory_table/', views.inventory_table, name='inventory_table'),
+    path('inventory_table_db/', views.inventory_table_from_db, name='inventory_table_db'),
+    path('inventory_table_api/', views.inventory_table_from_product_svc, name='inventory_table_api'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,17 +1,34 @@
 import math
 import time
+import logging
 
+import requests
+from pydantic import BaseModel
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from cyclecount.models import Inventory
 
 
+class ProductModel(BaseModel):
+    id: int
+    sku: str
+    description: str
+
+
 def list_inventory(request: HttpRequest) -> HttpResponse:
     return render(request, 'inventory/list_inventory.html', {})
 
 
-def inventory_table(request: HttpRequest) -> HttpResponse:
+'''
+WHY:
+Why we have two very similar methods in this view:
+  * inventory_table_from_db
+  * inventory_table_from_product_svc
+Both of these should spit out a very similar behavior.
+
+'''
+def inventory_table_from_db(request: HttpRequest) -> HttpResponse:
 
     page = int(request.GET.get('page')) - 1
     size = int(request.GET.get('size'))
@@ -27,11 +44,51 @@ def inventory_table(request: HttpRequest) -> HttpResponse:
         inventory = Inventory.objects.select_related('product', 'location').all().order_by('id')[start:end]
 
         inventory_records = []
-        for inv in inventory:  # [start:end]:
+        for inv in inventory:
             inventory_records.append({'id': inv.id, 'location': inv.location.description, 'sku': inv.product.sku, 'qty': inv.qty})
 
-    print(f'found {len(inventory_records)} many records of total:{inventory_count},',
-          f' slice on start-end:{start}-{end} sql_count_perf:{pt0.result * 1000} sql:{pt1.result * 1000}')
+    print(f'DB - found {len(inventory_records)} many records of total:{inventory_count}, ',
+          f'slice on start-end:{start}-{end} '
+          f'sql_count_perf:{int(pt0.result * 1000)} sql:{int(pt1.result * 1000)}')
+    result = {
+        'last_page': math.ceil(inventory_count / size),
+        'data': inventory_records
+    }
+    return JsonResponse(result)
+
+
+def inventory_table_from_product_svc(request: HttpRequest) -> HttpResponse:
+
+    page = int(request.GET.get('page')) - 1
+    size = int(request.GET.get('size'))
+    print(f'page:{page} size:{size}')
+
+    with PerfTrack() as pt0:
+        inventory_count = Inventory.objects.count()
+
+    start = size * page
+    end = start + size
+
+    logging.basicConfig(level=logging.DEBUG)
+    s = requests.Session()
+    #s.mount('http://', HTTPAdapter(max_retries=Retry(total=1)))
+
+    with PerfTrack() as pt1:
+        inventory = Inventory.objects.select_related('location').all().order_by('id')[start:end]
+
+        inventory_records = []
+        for inv in inventory:
+            r = s.get(f'http://127.0.0.1:8001/product/products/{inv.product_id}/')
+
+            if r.status_code == 200:
+                product = ProductModel(**r.json())
+                inventory_records.append({'id': inv.id, 'location': inv.location.description, 'sku': product.sku, 'qty': inv.qty})
+            else:
+                inventory_records.append({'id': inv.id, 'location': inv.location.description, 'sku': None, 'qty': inv.qty})
+
+    print(f'API - found {len(inventory_records)} many records of total:{inventory_count}, ',
+          f'slice on start-end:{start}-{end} '
+          f'sql_count_perf:{int(pt0.result * 1000)}ms get-data:{int(pt1.result * 1000)}ms')
     result = {
         'last_page': math.ceil(inventory_count / size),
         'data': inventory_records
