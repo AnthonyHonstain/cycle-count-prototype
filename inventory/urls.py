4e143231367e1dce643c4052a4103b25f025f9b9
Anthony Honstain <honstain@gmail.com>
2024-02-28 08:11:12 -0800

    Fix the url path for the optional local product lookup

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'inventory'
 urlpatterns = [
     path('list-inventory/', views.list_inventory, name='list_inventory'),
-    path('inventory-table_db/', views.inventory_table_from_db, name='inventory_table_db'),
+    path('inventory-table-db/', views.inventory_table_from_db, name='inventory_table_db'),
     path('inventory-table-api/', views.inventory_table_from_product_svc, name='inventory_table_api'),
 ]
